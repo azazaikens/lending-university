caaf3a9a44e1364fa26a9606286d9682a3afa579
azazaikens <rubanjuri@gmail.com>
2024-05-06 11:47:51 +0300

    add certificates

--- js/index.js
+++ js/index.js
@@ -1,33 +1,12 @@
-// const swiper = new Swiper(".swiper", {
-//   // Optional parameters
-//   direction: "vertical",
-//   loop: true,
-
-//   // If we need pagination
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: ".swiper-scrollbar",
-//   },
-// });
-
 var swiper = new Swiper(".swiper", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
   touchRatio: 2,
   slideToClickedSlide: true,
+  slidesPerView: 3,
+  initialSlide: 1,
 
-  slidesPerView: "auto",
   coverflowEffect: {
     rotate: 50,
     stretch: 0,
